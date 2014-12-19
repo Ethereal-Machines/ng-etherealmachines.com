8134580aa429347c0a3ddc56892c04205eb8444b
ethereal-machine <info.etherealmachine@gmail.com>
2014-12-19 21:30:35 +0530

    Preload icons test 2
    styling.js has been added to preload pictures on all pages.
    This is to test the work of "../" in selecting the right folder.

--- js/styling.js
+++ js/styling.js
@@ -1,7 +1,10 @@
-$(function()	{
-
-	$("#menu").children("li").mouseenter(function() {
-		$(this).animate({	'opacity': '0.5'	},500);
-	});
-
-});
+$(document).ready(function() {
+	tabs.init();
+	$.fn.preload = function() {
+		this.each(function(){
+			$('<img/>')[0].src = this;
+		});
+	}	
+	$(['../images/menu_icon1_active.gif','../images/menu_icon3_active.gif','../images/menu_icon4_active.gif','../images/menu_icon5_active.gif']).preload();
+	//MENU ICONS PRELOADED HERE
+})
