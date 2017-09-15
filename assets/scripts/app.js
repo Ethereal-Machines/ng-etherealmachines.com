69b8da7eb98cc13fc0d43f1f663f4e61afde790d
Kanishka Madhuni <kanishkamohanmadhuni@gmail.com>
2017-09-15 17:36:21 +0530

    Navigation target of the 'hero-image' is changed
    Extra bit of js code is modified

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,7 +4,13 @@ $(function() {
 		console.log("Click event registered on the links");
 		var target = $(this).attr('href');
 		var strip = target.slice(1);
-		var anchor = $("div[id='" + strip + "']");
+		var anchor;
+
+		if (strip !== "home") {
+			anchor = $("div[id='" + strip + "']");
+		} else {
+			anchor = $("header[id='" + strip + "']");
+		}
 
 		e.preventDefault();
 
