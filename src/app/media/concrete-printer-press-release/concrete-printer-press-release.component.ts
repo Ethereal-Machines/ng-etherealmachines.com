abd2cbd7ba1a2bb63de7eb66bb1983aba9e38d92
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2017-12-20 23:52:30 +0530

    Added press release page for concrete 3d printer

---
+++ src/app/media/concrete-printer-press-release/concrete-printer-press-release.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-concrete-printer-press-release',
+  templateUrl: './concrete-printer-press-release.component.html'
+})
+export class ConcretePrinterPressReleaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
