fc7ce0ac7bf43c4908b9719666b93744c2e9d34a
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-04-12 15:49:19 +0530

    Add options for the Scope and State for General Queries

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-contact-us',
@@ -11,6 +12,9 @@ export class ContactUsComponent implements OnInit {
   public natureOfEnquiry: string;
   public tagline: string;
   public showContactNum: boolean = false;
+  public showStates: boolean = false;
+  public showScope: boolean = false;
+  public currentScope: string;
 
   public tagLines = {
     concrete3dPrinter: 'To collaborate with us on Concrete 3d Printing!',
@@ -20,12 +24,28 @@ export class ContactUsComponent implements OnInit {
     becomeReseller: 'Write to us to become a reseller or distributor!'
   };
 
+  onScopeChange (e) {
+    this.currentScope = e.target.value;
+    if (this.currentScope === 'International') {
+      // console.log('Scope is ', this.currentScope, this.showStates);
+      this.showStates = false;
+    } else if (this.currentScope === 'India') {
+      // console.log('Scope is ', this.currentScope, this.showStates);
+      this.showStates = true;
+    }
+  }
+
   changeContentOnLinkChange() {
     const link: string = window.location.pathname;
 
     if (link.indexOf('general-queries') !== -1) {
       this.natureOfEnquiry = 'General Queries';
       this.tagline = this.tagLines.generalQueries;
+      // this.showStates = false;
+      this.showScope = true;
+      setTimeout(() => {
+        $('#scope').change(this.onScopeChange.bind(this));
+      }, 1000);
     } else if (link.indexOf('careers') !== -1) {
       this.natureOfEnquiry = 'Careers';
       this.tagline = this.tagLines.careers;
@@ -45,5 +65,4 @@ export class ContactUsComponent implements OnInit {
   ngOnInit() {
     this.changeContentOnLinkChange();
   }
-
 }
